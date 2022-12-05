d2d0d493b23e48326331f131b7e8f8c21483664e
kareem89 <muslimkareem89@icloud.com>
2022-12-05 09:17:20 +0100

    delete tests in ProductRepoTest

--- src/test/java/org/example/repo/ProductRepoTest.java
+++ src/test/java/org/example/repo/ProductRepoTest.java
@@ -12,33 +12,12 @@ class ProductRepoTest {
 
     @Test
     void listProducts() {
-        //Given
-                List<Product> productList = new ArrayList<>(List.of(
-                                            new Product("1","Kikse"),
-                                            new Product("1","Quak"),
-                                            new Product("1","Kikse")
-        ));
-            //When
-        ProductRepo productRepo = new ProductRepo(productList);
-        String actual = productRepo.listProducts();
 
-        //Then
-        assertEquals("""
-                Product Name | Product Id
-                Kikse | 1
-                Quak | 1
-                Kikse | 1
-                """,actual);
     }
 
     @Test
     void listProductsWhenIsEmpty() {
 
-        //When
-        ProductRepo productRepo = new ProductRepo(new ArrayList<>());
-        String actual = productRepo.listProducts();
 
-        //Then
-        assertEquals("",actual);
     }
 }
